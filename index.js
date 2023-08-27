19c7d7d49bc09a91cccc6ed93548bf2bfed13874
Maryybactista <142637042+Maryybactista@users.noreply.github.com>
2023-08-27 17:14:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,208 @@
+let titulo = document.querySelector('h1')
+let instrucoes = document.querySelector('#instrucoes')
+let aviso = document.querySelector('#aviso')
+//let respostaEsta = document.querySelector('#respostaEsta')
+let pontos = 0 // pontos para o placar
+let placar = 0 // placar
+
+// PERGUNTA
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta   = document.querySelector('#pergunta')
+
+// ALTERNATIVAS
+let a = document.querySelector('#a')
+let b = document.querySelector('#b')
+let c = document.querySelector('#c')
+let d = document.querySelector('#d')
+
+// article com a class questoes
+let articleQuestoes = document.querySelector('.questoes')
+// ol li com as alternativas
+let alternativas = document.querySelector('#alternativas')
+
+const q0 = {
+    numQuestao   : 0,
+    pergunta     : "Pergunta",
+    alternativaA : "Alternativa A",
+    alternativaB : "Alternativa B",
+    alternativaC : "Alternativa C",
+    alternativaD : "Alternativa D",
+    correta      : "0",
+}
+
+const q1 = {
+    numQuestao   : 1,
+    pergunta     : "Qual a fórmula para calcular a área de um triângulo?",
+    alternativaA : " A = l x l",
+    alternativaB : "A = b x h ",
+    alternativaC : "A = (b x h)/2 ",
+    correta      : "A=(bxh)/2",
+}
+
+const q2 = {
+    numQuestao   : 2,
+    pergunta     : "Qual é o resultado de 10 - 6 x 2?",
+    alternativaA : "2",
+    alternativaB : "-2",
+    alternativaC : "12",
+    
+    correta      : "-2",
+}
+
+const q3 = {
+    numQuestao   : 3,
+    pergunta     : "Qual é o resultado de 17 + 8 - 3?",
+    alternativaA : "12",
+    alternativaB : "22",
+    alternativaC : "32",
+    alternativaD : "46",
+    correta      : "22",
+}
+
+const q4 = {
+    numQuestao   : 4,
+    pergunta     : "Dois pais e dois filhos sentaram-se para comer ovos no café da manhã. Cada um comeu um ovo. Quanto os ovos eles comeram no total?",
+    alternativaA : "1",
+    alternativaB : "3",
+    alternativaC : "5",
+    alternativaD : "8",
+    correta      : "3",
+}
+
+const q5 = {
+    numQuestao   : 5,
+    pergunta     : " Andando por uma rua, um homem conta 10 árvores à sua direita. Na volta, conta 10 árvores à sua esquerda. Quantas árvores ele viu no total?",
+    alternativaA : "10",
+    alternativaB : "20",
+    alternativaC : "30",
+    alternativaD : "40",
+    correta      : "10",
+}
+
+// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
+const questoes = [q0, q1, q2, q3, q4, q5]
+
+let numero = document.querySelector('#numero')
+let total  = document.querySelector('#total')
+
+numero.textContent = q1.numQuestao
+
+let totalDeQuestoes = (questoes.length)-1
+console.log("Total de questões " + totalDeQuestoes)
+total.textContent = totalDeQuestoes
+
+// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
+numQuestao.textContent = q1.numQuestao
+pergunta.textContent   = q1.pergunta
+a.textContent = q1.alternativaA
+b.textContent = q1.alternativaB
+c.textContent = q1.alternativaC
+d.textContent = q1.alternativad
+
+
+// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
+a.setAttribute('value', '1A')
+b.setAttribute('value', '1B')
+c.setAttribute('value', '1C')
+d.setAttribute('value', '1C')
+
+
+// PARA MONTAR AS PROXIMAS QUESTOES
+function proximaQuestao(nQuestao) {
+    numero.textContent = nQuestao
+    numQuestao.textContent = questoes[nQuestao].numQuestao
+    pergunta.textContent   = questoes[nQuestao].pergunta
+    a.textContent = questoes[nQuestao].alternativaA
+    b.textContent = questoes[nQuestao].alternativaB
+    c.textContent = questoes[nQuestao].alternativaC
+    d.textContent = questoes[nQuestao].alternativaC
+    a.setAttribute('value', nQuestao+'A')
+    b.setAttribute('value', nQuestao+'B')
+    c.setAttribute('value', nQuestao+'C')
+    d.setAttribute('value', nQuestao+'D')
+}
+
+function bloquearAlternativas() {
+    a.classList.add('bloqueado')
+    b.classList.add('bloqueado')
+    c.classList.add('bloqueado')
+    d.classList.add('bloqueado')
+}
+
+function desbloquearAlternativas() {
+    a.classList.remove('bloqueado')
+    b.classList.remove('bloqueado')
+    c.classList.remove('bloqueado')
+    d.classList.remove('bloqueado')
+}
+
+function verificarSeAcertou(nQuestao, resposta) {
+
+    let numeroDaQuestao = nQuestao.value
+    console.log("Questão " + numeroDaQuestao)
+
+    let respostaEscolhida = resposta.textContent
+    //console.log("RespU " + respostaEscolhida)
+
+    let certa = questoes[numeroDaQuestao].correta
+    //console.log("RespC " + certa)
+
+    if(respostaEscolhida == certa) {
+        //console.log("Acertou")
+        //respostaEsta.textContent = "Correta 😊"
+        pontos += 10 // pontos = pontos + 10
+    } else {
+        //console.log("Errou!")
+        //respostaEsta.textContent = "Errada 😢"
+    }
+
+    // atualizar placar
+    placar = pontos
+    instrucoes.textContent = "Pontos " + placar
+
+    // bloquear a escolha de opcoes
+    bloquearAlternativas()
+
+    setTimeout(function() {
+        //respostaEsta.textContent = '...'
+        proxima = numeroDaQuestao+1
+
+        if(proxima > totalDeQuestoes) {
+            console.log('Fim do Jogo!')
+            fimDoJogo()
+        } else {
+            proximaQuestao(proxima)
+        }
+    }, 250)
+    desbloquearAlternativas()
+}'  \z ,.'
+
+function fimDoJogo() {
+    instrucoes.textContent = "Fim de Jogo!"
+    numQuestao.textContent = ""
+
+    let pont = ''
+    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
+
+    pergunta.textContent   = "Você conseguiu " + pontos + " " + pont
+
+    aviso.textContent = "Você conseguiu " + pontos + " " + pont
+
+    a.textContent = ""
+    b.textContent = ""
+    c.textContent = ""
+    d.textContent = ""
+
+    a.setAttribute('value', '0')
+    b.setAttribute('value', '0')
+    c.setAttribute('value', '0')
+    d.setAttribute('value', '0')
+
+    // OCULTAR O ARTICLE DA QUESTAO
+    articleQuestoes.style.display = 'none'
+
+    setTimeout(function() {
+        pontos = 0 // zerar placar
+        location.reload();
+    }, 2000)
+}
